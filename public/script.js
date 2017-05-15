b11618a3618b09fc53c175c9320c7816b04c56a0
AutumnCharnesky <autumn.breeze.c18@gmail.com>
2017-05-15 13:37:31 -0400

    css and html

---
+++ public/script.js
@@ -0,0 +1,36 @@
+var app = angular.module('appMod', []);
+
+app.controller ('apiCtrl', function($scope, apifactory){
+  console.log('api controller check');
+
+  apifactory.setStuff().then(function() {
+    $scope.stuffOnPage=apifactory.getStuff();
+  });
+
+  });
+
+  app.factory('apifactory', function($http){
+    console.log('factory has loaded');
+    var stuff=[];
+
+    function setStuff() {
+      var promise=$http({
+        method: "GET",
+        url: '/stuff',
+      }) .then(function callSeuccess(response) {
+        stuff = response.data;
+      },function(error) {
+        console.log(error);
+      })
+      return promise;
+      };
+
+    function getStuff() {
+      return stuff;
+    };
+
+    return {
+      setStuff: setStuff,
+      getStuff: getStuff
+    };
+  });
